71f9e2d4599700737d2dd61cd9f3b1100214701a
nta1998 <gerunds-crape.0k@icloud.com>
2023-01-23 13:27:33 +0200

    fix

--- front/test/src/app/store.ts
+++ front/test/src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/studentsSlice';
+import counterReducer from '../features/students/studentsSlice';
 
 export const store = configureStore({
   reducer: {
